4a795fcfe35e66e659e143a474293c3493874250
orad-cohen <57364882+orad-cohen@github.com>
2019-12-02 13:45:08 +0200

    Added tests

--- Test/Ex1Testing/MonomTest.java
+++ Test/Ex1Testing/MonomTest.java
@@ -4,12 +4,10 @@ import Ex1.Monom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MonomTest {
-    public static void main(String[] args){
 
-    }
     @BeforeEach
     void setUp() {
 
@@ -22,23 +20,57 @@ class MonomTest {
         Monom m1 = new Monom("5");
         Monom m2 = new Monom("3x^2");
         Monom m3 = new Monom("6x");
-        assertEquals(m2.derivative(), m3);
+        assertEquals(m2.derivative().toString(), m3.toString());
+        assertEquals("0",m1.derivative().toString());
 
     }
-
+    @Test
     void testF() {
+        Monom m1 = new Monom("3x^5");
+        Monom m2 = new Monom("6");
+        Monom m3 = new Monom("4x");
+        assertEquals(m1.f(2.4),238.87872,0.00001);
+        assertEquals(m2.f(10),6);
+        assertEquals(m3.f(7.2),28.8);
     }
-
+    @Test
     void testIsZero() {
+        Monom m1 = new Monom("0x^3");
+        Monom m2 = new Monom("0");
+        assertEquals(m1.toString(),Monom.ZERO.toString());
+        assertEquals(m2.toString(),m1.toString());
     }
-
+    @Test
     void testAdd() {
+        Monom m1 = new Monom("3");
+        Monom m2 = new Monom("8");
+        Monom m3 = new Monom("2x^3");
+        Monom m4 = new Monom("4x^3");
+        m1.add(m2);
+        m3.add(m4);
+        assertEquals(m1.toString(),new Monom("11").toString());
+        assertEquals(m3.toString(),new Monom("6x^3").toString());
     }
-
+    @Test
     void testMultiply() {
+        Monom m1 = new Monom("3");
+        Monom m2 = new Monom("8");
+        Monom m3 = new Monom("2x^3");
+        Monom m4 = new Monom("4x^3");
+        m1.multiply(m3);
+        m3.multiply(m4);
+        m2.multiply(m4);
+        assertEquals(m1.toString(),"6.0x^3");
+        assertEquals(m3.toString(),"8.0x^6");
+        assertEquals(m2.toString(),"16x^3");
+
     }
 
     void testToString() {
+        Monom m3 = new Monom("2x^3");
+        Monom m4 = new Monom("4x^3");
+        assertEquals("2.0x^3",m3.toString());
+        assertEquals("4.0x^3",m4.toString());
     }
 
     void testRoundAvoid() {
